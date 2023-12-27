f8a894815fb47460670a592ce44a9b5ed4720ae4
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-27 07:13:07 +0530

    render the list items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Chargee", quantity: 2, packed: true },
+];
+
 function App() {
   return (
     <div className="App">
@@ -22,7 +28,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
