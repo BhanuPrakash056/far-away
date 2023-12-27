f25e69b7940078dc8c319c81d293d7a8a29b886a
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-27 17:23:14 +0530

    add form and handleSubmit

--- src/App.js
+++ src/App.js
@@ -20,10 +20,23 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want for your trips ?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {" "}
+            {num}{" "}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button type="submit">Add</button>
+    </form>
   );
 }
 
