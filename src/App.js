c2e27fc3d0fdba59321cdcbbdbdd2b80e3acd8d0
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-12-28 11:45:31 +0530

    removed initial items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 2, packed: true },
-];
 
 function App() {
   const [items, setItems] = useState([])
